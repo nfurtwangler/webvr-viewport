cb90ff413ef7258999df20e4df881c783ff7aca2
nfurtwangler <nfurtwangler@gmail.com>
2017-06-18 12:41:29 -0700

    Deleting bin from repo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var static = require('node-static');
 var file = new static.Server();
-console.log('Creating static server');
+var port = process.env.PORT || 3000;
+
+console.log('Creating static server on port:  ' + port);
+
 require('http').createServer(function(request, response) {
-  console.log('request received');
   request.addListener('end', function() {
     file.serve(request, response);
   }).resume();
-}).listen(process.env.PORT || 3000);
+}).listen(port);
