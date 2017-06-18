17dc457a22f3a94235687192c6f8f6392319e1bf
nfurtwangler <nfurtwangler@gmail.com>
2017-06-18 12:37:26 -0700

    Fixing dev dependency

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var static = require('node-static');
 var file = new static.Server();
+console.log('Creating static server');
 require('http').createServer(function(request, response) {
+  console.log('request received');
   request.addListener('end', function() {
     file.serve(request, response);
   }).resume();
