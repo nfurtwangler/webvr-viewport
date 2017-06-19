248d905d933e4e727d7c340693cf9e0f6043c9b7
nfurtwangler <nfurtwangler@gmail.com>
2017-06-18 19:16:12 -0700

    Cleanup unused configs

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const static = require('node-static');
-const https = require('http');
+const http = require('http');
 const WebSocket = require('ws');
 const WebSocketServer = WebSocket.Server;
 
 const file = new static.Server();
 const port = process.env.PORT || 3000;
 
-/*const serverConfig = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
-*/
-
 console.log('Creating static server on port:  ' + port);
-const httpsServer = https.createServer(function(request, response) {
+const httpServer = http.createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
   }).resume();
 });
-httpsServer.listen(port);
+httpServer.listen(port);
 
-console.log('Creating WebSocket server');
-const wss = new WebSocketServer({server: httpsServer});
-
+console.log('Attaching WebSocket server');
+const wss = new WebSocketServer({ server: httpServer });
 wss.on('connection', function(ws) {
   ws.on('message', function(message) {
     // Broadcast any received message to all clients
@@ -30,7 +23,6 @@ wss.on('connection', function(ws) {
     wss.broadcast(message);
   });
 });
-
 wss.broadcast = function(data) {
   this.clients.forEach(function(client) {
     if (client.readyState === WebSocket.OPEN) {
